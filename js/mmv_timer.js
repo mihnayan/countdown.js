b5e72635b8cb5a3af82adf9c01c53fe94eb07937
Mikhail Mangushev <dev.mihnayan@gmail.com>
2014-04-02 23:57:44 +0400

    Events data structure was changed (actions separated). Actions executing implemented.

--- js/mmv_timer.js
+++ js/mmv_timer.js
@@ -5,21 +5,33 @@
  * @version 0.1
  */
 
+ /* TODO:
+1. Сделать работоспособные действия
+2. Необходимо реализовать события таймера так, чтобы они корректно отрабатывлись, если на события
+   подписываются из разных мест.
+*/
+
  var events = [
     {
-        timeout: 1396293177000,
-        hide: [1, 2],
-        show: [3, 4, 5],
+        timeout: 1396468371000,
+        actions: {
+            hide: ["block-1", "block-2"],
+            show: ["block-3", "block-4", "block-5"],
+        },
     },
     {
-        timeout: 1396293207000,
-        hide: [3, 4, 5],
-        show: [1,2],
+        timeout: 1396468401000,
+        actions: {
+            hide: ["block-3", "block-4", "block-5"],
+            show: ["block-1", "block-2"],
+        },
     },
     {
-        timeout: 1395296820000,
-        hide: [1, 2],
-        show: [3, 4, 5],
+        timeout: 1396468431000,
+        actions: {
+            hide: ["block-1", "block-2"],
+            show: ["block-3", "block-4", "block-5"],
+        },
     },
  ];
 
@@ -38,6 +50,7 @@ var timer = function (events, actions) {
             setTimeout(function() { count(timeout); }, 1000);
         } else {
             if (typeof self.onstartevent == 'function') self.onstartevent(currentEvent);
+            setTimeout(execute(events[currentEvent].actions), 10);
             if (currentEvent < (events.length - 1)) {
                 currentEvent++;
                 if (typeof self.onchangeevent == 'function') self.onchangeevent(currentEvent);
@@ -47,20 +60,26 @@ var timer = function (events, actions) {
         console.log(timeout - Date.parse(new Date()));
     }
 
-    /* 
+    var execute = function(acts) {
+        for (act in acts) {
+            actions[act](acts[act]);
+        }
+    };
+
+    /**
      * Событие, возникающее при каждом отсчете времени (раз в секунду)
      * @param timeout время, оставшееся до начала текущего события
      */
     this.ontick = function (timeout) {};
 
-    /*
+    /**
      * Событие, возникающее при начале мероприятия (в данном случае заменено слово "событие"
      * для устранения тавтологии).
      * @param eventId идентификатор события (мероприятия), начало которого произошло
      */
     this.onstartevent = function (eventId) {};
 
-    /*
+    /**
      * Событие, возникающее при смене текущего мероприятия после того, как наступило предыдущее.
      * @param eventId идентификатор текущего мероприятия
      */
@@ -71,28 +90,27 @@ var timer = function (events, actions) {
     };
 };
 
-var executor = function() {
-    
-};
-
 // -------------- действия, доступные для выполнения в событиях
+var actions = {
+    /**
+    * Скрывает элементы, идентификаторы которых переданы в массиве elements
+    */
+    hide: function (elements) {
+        console.log("hide ations started");
+        for (var i = 0; i < elements.length; i++) {
+            var e = document.getElementById(elements[i]);
+            if (e != null) e.style.display = 'none';
+        }
+    },
 
-/**
- * Скрывает элементы, идентификаторы которых переданы в массиве elements
- */
-var hide_action = function(elements) {
-    for (var i = 0; i < elements.length; i++) {
-        var e = document.getElementById(elements[i]);
-        if (e != null) e.style.display = 'none';
-    }
-};
-
-/**
- * Делает видимыми элементы, идентификаторы которых переданы в массиве show_array
- */
-var show_action = function(elements) {
-    for (var i = 0; i < elements.length; i++) {
-        var e = document.getElementById(elements[i]);
-        if (e != null) e.style.display = 'block';
-    }
+    /**
+    * Делает видимыми элементы, идентификаторы которых переданы в массиве show_array
+    */
+    show: function(elements) {
+        console.log("show action started");
+        for (var i = 0; i < elements.length; i++) {
+            var e = document.getElementById(elements[i]);
+            if (e != null) e.style.display = 'block';
+        }
+    },
 };
